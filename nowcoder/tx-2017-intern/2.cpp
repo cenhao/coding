57a44916ab09eb1635d44e0feb57695a53d398df
cenhao <cenhao123@gmail.com>
2016-09-18 16:56:31 -0700

    nowcoder tx 2017 intern Q2 - Simple string

---
+++ nowcoder/tx-2017-intern/2.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAXS 1000
+
+char str[MAXS+1];
+
+int main() {
+	while (EOF != scanf("%s", str)) {
+		int len = strlen(str);
+		for (int i=len-1; i>=0; --i) {
+			if (str[i] <= 'Z') {
+				str[len] = str[i];
+				int i2;
+				for (i2=i+1; i2<len && str[i2]>='a'; ++i2) {
+					str[i2-1] = str[i2];
+				}
+				str[i2-1] = str[len];
+			}
+		}
+
+		str[len] = 0;
+		printf("%s\n", str);
+	}
+}
