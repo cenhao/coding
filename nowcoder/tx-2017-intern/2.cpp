5284f93c93584c2740a546ee5dbe263152db6319
cenhao <cenhao123@gmail.com>
2016-09-18 17:48:01 -0700

    nowcoder tx 2017 intern Q3 - Math. Adding correct solution for 2

--- nowcoder/tx-2017-intern/2.cpp
+++ nowcoder/tx-2017-intern/2.cpp
@@ -1,3 +1,4 @@
+/*
 #include <stdio.h>
 #include <string.h>
 
@@ -22,4 +23,26 @@ int main() {
 		str[len] = 0;
 		printf("%s\n", str);
 	}
+}
+*/
+
+// The correct solution
+#include <stdio.h>
+
+#define MAXS 1000
+
+char str[MAXS+1];
+
+int main() {
+	while (EOF != scanf("%s", str)) {
+		for (int i=0; str[i]!=0; ++i) {
+			if (str[i] >= 'a') { printf("%c", str[i]); }
+		}
+		for (int i=0; str[i]!=0; ++i) {
+			if (str[i] <= 'Z') { printf("%c", str[i]); }
+		}
+		printf("\n");
+	}
+
+	return 0;
 }
---
+++ nowcoder/tx-2017-intern/3.cpp
@@ -0,0 +1,75 @@
+#include <algorithm>
+using namespace std;
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAXN 100000
+
+long long a[MAXN+1];
+int n;
+
+inline long long calcCn2(long long x) {
+	return x * (x-1) / 2;
+}
+
+long long processMin() {
+	long long mn = 0x8f000000LL;
+	long long res = 0;
+	for (int i=1; i<n; ++i) { mn = min(a[i]-a[i-1], mn); }
+
+	if (mn == 0) {
+		long long last = -1;
+		long long cnt = 0;
+		a[n] = -2;
+
+		for (int i=0; i<=n; ++i) {
+			if (a[i] != last) {
+				if (cnt > 1) {
+					res += calcCn2(cnt);
+				}  
+				cnt = 1;
+				last = a[i];
+			} else {
+				++cnt;
+			}  
+		}  
+	} else {
+		for (int i=1; i<n; ++i) {
+			if (a[i] - a[i-1] == mn) { ++res; }
+		}  
+	}  
+
+	return res;
+}
+
+long long processMax() {
+	long long mx = a[n-1] - a[0];
+	if (mx == 0) { return calcCn2(n); }
+	long long zcnt = 0, ecnt = 0;
+	for (int i=0; i<n; ++i) {
+		if (a[i] == a[0]) { ++zcnt; }
+		else if (a[i] == a[n-1]) { ++ecnt; }
+	}  
+
+	return zcnt * ecnt;
+}
+
+void process() {
+	printf("%lld %lld\n", processMin(), processMax());
+}
+
+int main() {
+	while (EOF != scanf("%d", &n)) {
+		for (int i=0; i<n; ++i) { scanf("%lld", &a[i]); }
+		if (n == 1) {
+			printf("0 0\n");
+			continue;
+		}
+
+		sort(a, a+n);
+		process();
+	}
+
+	return 0;
+}
