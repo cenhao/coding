0d83306a1d4f862c00af7ed6287f02b01711411d
cenhao <cenhao123@gmail.com>
2016-11-04 20:44:09 -0700

    leetcode 94 - simple tree

---
+++ leet/0100/094.cpp
@@ -0,0 +1,26 @@
+#include <cstdlib>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+	public:
+		vector<int> inorderTraversal(TreeNode* root) {
+			vector<int> result;
+			inorder(root, result);
+			return result;
+		}
+
+		void inorder(TreeNode* node, vector<int> &result) {
+			if (node == NULL) { return ; }
+			inorder(node->left, result);
+			result.push_back(node->val);
+			inorder(node->right, result);
+		}
+};
