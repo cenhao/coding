ee3877200a345bf3a148482ba65436597ab6ef44
cenhao <cenhao123@gmail.com>
2017-02-02 01:20:31 -0800

    leetcode 138 - copy linked list with random pointer, 3 pass

---
+++ leet/0150/138.cpp
@@ -0,0 +1,60 @@
+#include <cstdlib>
+using namespace std;
+
+struct RandomListNode {
+	int label;
+	RandomListNode *next, *random;
+	RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+};
+
+class Solution {
+	public:
+	RandomListNode *copyRandomList(RandomListNode *head) {
+		if (head == NULL) { return NULL; }
+
+		RandomListNode *clone = NULL;
+		for (RandomListNode *cur=head; cur!=NULL; cur=cur->next->next) {
+			RandomListNode *pn;
+			if (clone == NULL) {
+				clone = pn = new RandomListNode(cur->label);
+			} else {
+				pn = new RandomListNode(cur->label);
+			}
+
+			pn->next = cur->next;
+			cur->next = pn;
+		}
+
+		for (RandomListNode *cur=head; cur!=NULL; cur=cur->next->next) {
+			RandomListNode *pn = cur->next;
+			pn->random = cur->random == NULL ? NULL : cur->random->next;
+		}
+
+		for (RandomListNode *cur=head; cur!=NULL; cur=cur->next) {
+			RandomListNode *pn = cur->next;
+			RandomListNode *tmp = pn->next;
+			pn->next = tmp == NULL ? NULL : tmp->next;
+			cur->next = tmp;
+		}
+
+		return clone;
+	}
+};
+
+int main() {
+	RandomListNode rn(-1);
+	RandomListNode rn1(8);
+	RandomListNode rn2(7);
+	RandomListNode rn3(-3);
+	RandomListNode rn4(4);
+	rn.next = &rn1;
+	rn1.next = &rn2;
+	rn2.next = &rn3;
+	rn3.next = &rn4;
+	rn.random = &rn4; rn4.random = &rn;
+	rn1.random = &rn3;
+	Solution sol;
+	RandomListNode *pn = sol.copyRandomList(&rn);
+	delete pn;
+	return 0;
+}
